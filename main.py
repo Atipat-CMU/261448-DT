a2d85714b4ef6f393d47ffe3c5a5d393ba61c409
Atipat D <95071528+Atipat-CMU@users.noreply.github.com>
2024-02-03 14:51:23 +0700

    test: add more datasets

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import dotlis as dl
 import datkit
 
-table = dl.read_tsv("buycom.txt")
+table = dl.read_tsv("car.txt")
 
-X, y = table.exclass("buys_computer")
+X, y = table.exclass("class")
 
 clf = datkit.DecisionTree()
 clf = clf.fit(X, y)
 
 clf.print_tree()
+
+# print(clf.predict_one(["5.3...6.3","<=2.8","2.4...4.8","0.7...1.7"]))
+# print(clf.predict(X))
