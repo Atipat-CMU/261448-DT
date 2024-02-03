cb6cd5dbcce294bc2cf0577bddfccf2aa147a196
Atipat D <95071528+Atipat-CMU@users.noreply.github.com>
2024-02-03 14:51:05 +0700

    datkit: fix bug and add predict function in decision tree

--- datkit.py
+++ datkit.py
@@ -1,5 +1,6 @@
 import math
 from collections import deque
+import copy
 
 class DecisionTree:
     def __init__(self):
@@ -14,17 +15,28 @@ class DecisionTree:
         queue = deque([self.root])
         while queue:
             node = queue.popleft()
-            if(node.children != []):
+            if(node.children != {}):
                 print(node)
-            for child in node.children:
-                queue.append(child["node"])
+            for value in node.children:
+                queue.append(node.children[value])
+
+    def predict_one(self, point):
+        current = self.root
+        attributes = self.root.X.attributes
+        while current.answer == "":
+            index = attributes.index(current.select_attr)
+            current = current.children[point[index]]
+        return current.answer
+    
+    def predict(self, data):
+        return [self.predict_one(point) for point in data.rows]
 
 class Node:
     def __init__(self):
         self.X = None
         self.y = None
         self.info = 0
-        self.children = []
+        self.children = {}
         self.data = {}
         self.parent = ""
         self.select_attr = ""
@@ -32,34 +44,36 @@ class Node:
     
     def __str__(self):
         string = []
-        string.append("--------------------------------\n")
+        string.append("--------------------------------------------\n")
         if(self.parent != ""):
-            string.append("From - [" + self.parent + "]\n")
-            string.append("--------------------------------\n")
+            string.append("> From - [" + self.parent + "]\n")
+            string.append("--------------------------------------------\n")
         else:
-            string.append("Start\n")
-            string.append("--------------------------------\n")
-        if(self.children != []): string.append(self.select_attr + "?\n")
-        for child in self.children:
-            string.append(child["value"] + ": ")
-            if(child["node"].children != []):
-                string.append(child["node"].select_attr)
-            else: string.append(child["node"].answer)
+            string.append("> Start\n")
+            string.append("--------------------------------------------\n")
+        if(self.children != {}): string.append(" " + self.select_attr + "?\n")
+        # string.append("--------------------------------\n")
+        for value in self.children:
+            string.append("  + " + value)
+            if(self.children[value].children != {}):
+                string.append(" -> " + self.children[value].select_attr)
+            else: string.append(": " + self.children[value].answer)
             string.append("\n")
-        string.append("--------------------------------\n")
+        string.append("--------------------------------------------\n")
         return ''.join(string)
         
-    def fit(self, X, y):
+    def fit(self, X, y, table=None):
         self.X = X
         self.y = y
         self.info = self.__cal_info(self.y.column(0))
-        self.__prepare_data()
-        self.__count_data()
-        self.__info_data()
+        if(table == None): table = self.__create_table()
+        self.data = copy.deepcopy(table)
+        self.data = self.__count_data(self.data)
+        self.data = self.__info_data(self.data)
 
         max_gain = 0
         for attr in self.data:
-            if self.data[attr]["gain"] > max_gain:
+            if self.data[attr]["gain"] >= max_gain:
                 self.select_attr = attr
                 max_gain = self.data[attr]["gain"]
         
@@ -69,54 +83,61 @@ class Node:
             if(self.data[self.select_attr]["values"][value]["info"] == 0):
                 values = self.data[self.select_attr]["values"][value]["classes"]
                 child.answer = max(values, key=values.get)
+            elif(len(self.X.attributes) == 1):
+                values = self.data[self.select_attr]["values"][value]["classes"]
+                child.answer = max(values, key=values.get)
             else:
+                child_table = copy.deepcopy(table)
+                child_table.pop(self.select_attr)
                 new_X = X.select(X.equal(self.select_attr, value)).drop_column([self.select_attr])
                 new_y = y.select(X.equal(self.select_attr, value))
-                child.fit(new_X, new_y)
-            self.children.append({"value": value, "node": child})
-    
-    def __prepare_data(self):
-        self.data = {}
+                child.fit(new_X, new_y, child_table)
+            self.children[value] = child
+
+    def __create_table(self):
+        data = {}
         for i in range(len(self.X.attributes)):
             attr_name = self.X.attributes[i]
-            self.data[attr_name] = {}
-            self.data[attr_name]["values"] = {}
+            data[attr_name] = {}
+            data[attr_name]["values"] = {}
             values = list(set(self.X.column(i)))
 
             for j in range(len(values)):
                 value_name = values[j]
-                self.data[attr_name]["values"][value_name] = {}
-                self.data[attr_name]["values"][value_name]["count"] = 0
-                self.data[attr_name]["values"][value_name]["classes"] = {}
+                data[attr_name]["values"][value_name] = {}
+                data[attr_name]["values"][value_name]["count"] = 0
+                data[attr_name]["values"][value_name]["classes"] = {}
                 for label in set(self.y.column(0)):
-                    self.data[attr_name]["values"][value_name]["classes"][label] = 0
+                    data[attr_name]["values"][value_name]["classes"][label] = 0
+        return data
 
-    def __count_data(self):
+    def __count_data(self, data):
         attributes = self.X.attributes
         rows = self.X.rows
         for n in range(len(rows)):
             attributes
             for i in range(len(attributes)):
                 label = self.y.column(0)[n]
-                self.data[attributes[i]]["values"][rows[n][i]]["classes"][label] += 1
-                self.data[attributes[i]]["values"][rows[n][i]]["count"] += 1
+                data[attributes[i]]["values"][rows[n][i]]["classes"][label] += 1
+                data[attributes[i]]["values"][rows[n][i]]["count"] += 1
+        return data
 
-    def __info_data(self):
+    def __info_data(self, data):
         attributes = self.X.attributes
         for i in range(len(attributes)):
             attr_name = attributes[i]
-            values = list(set(self.X.column(i)))
             attr_info = 0
-            for value_name in values:
+            for value_name in data[attr_name]["values"]:
                 info = 0
-                space = self.data[attr_name]["values"][value_name]["count"]
-                for label in self.data[attr_name]["values"][value_name]["classes"]:
-                    event = self.data[attr_name]["values"][value_name]["classes"][label]
+                space = data[attr_name]["values"][value_name]["count"]
+                for label in data[attr_name]["values"][value_name]["classes"]:
+                    event = data[attr_name]["values"][value_name]["classes"][label]
                     info += self.__entropy(event, space)
-                self.data[attr_name]["values"][value_name]["info"] = info = -1*info
+                data[attr_name]["values"][value_name]["info"] = info = -1*info
                 attr_info += space/len(self.X.rows)*info
-            space = self.data[attr_name]["info"] = attr_info
-            space = self.data[attr_name]["gain"] = self.info - attr_info
+            space = data[attr_name]["info"] = attr_info
+            space = data[attr_name]["gain"] = self.info - attr_info
+        return data
     
     def __cal_info(self, y):
         classes = set(y)
